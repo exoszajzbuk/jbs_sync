09d04090ed05637d10063f8d784d8c7db848c3c1
Balazs KOVACS <exoszajzbuk@gmail.com>
2012-02-22 18:29:19 +0100

    enh: testlogin added

--- app/controllers/synchronizations_controller.rb
+++ app/controllers/synchronizations_controller.rb
@@ -20,24 +20,55 @@ class SynchronizationsController < ApplicationController
   end
   
   # ------------------------------
+  # test login
+  
+  def testlogin
+    return unless testauthenticate == true
+    
+    render :nothing => true
+  end
+
+  def testauthenticate
+    # authenticate
+    authenticated = authenticate_or_request_with_http_basic "Authentication Required" do |username, password|
+      user = User.find_by_username(username)
+      username == user.username && user.valid_password?(password)
+    end
+    if authenticated == true
+      true
+    else
+      false
+  end
+  
+  # ------------------------------
   # custom synchronization methods
   
   # update sync a whole model
   def update_all
     if @model.needs_authentication? then
-      render :text => t('needs_authentication')
+      update_all_authenticated
     else
-      
-      if params[:updated_at].nil? then
-        @records = @model.all
-      else
-        @records = @model.find(:all, :conditions => ['updated_at > ?', Time.parse(params[:updated_at])+1])
-      end
-      
-      respond_with_records @records
+      update_all_unauthenticated
     end
   end
   
+  def update_all_unauthenticated
+    if params[:updated_at].nil? then
+      @records = @model.all
+    else
+      @records = @model.find(:all, :conditions => ['updated_at > ?', Time.parse(params[:updated_at])+1])
+    end
+        
+    respond_with_records @records    
+  end
+  
+  def update_all_authenticated    
+    return unless testauthenticate == true
+    
+    # render content
+    render :text => "authenticated stuff"
+  end
+    
   # delete sync a whole model
   def delete_all
     if @model.needs_authentication? then
@@ -50,36 +81,27 @@ class SynchronizationsController < ApplicationController
   end
   
   # down sync one record
-  def update_one
-    if @model.needs_authentication? then
-      update_all_authenticated
-    else
-      update_one_unauthenticated
-    end
-  end
+  #def update_one
+  #  if @model.needs_authentication? then
+  #    update_one_authenticated
+  #  else
+  #    update_one_unauthenticated
+  #  end
+  #end
   
-  def update_one_unauthenticated
-    @record = @model.find(params[:id])
-    respond_to do |format|
-      format.html { render :text => t('format_error') }
-      format.json { render :json => @record }
-      format.xml { render :xml => @record }
-    end
-  end
+  #def update_one_unauthenticated
+  #  @record = @model.find(params[:id])
+  #  respond_to do |format|
+  #    format.html { render :text => t('format_error') }
+  #    format.json { render :json => @record }
+  #    format.xml { render :xml => @record }
+  #  end
+  #end
   
-  def update_all_authenticated
-    user = User.find(params[:id])
-    
-    # authenticate
-    authenticated = authenticate_or_request_with_http_basic "Authentication Required" do |username, password|
-      username == user.username && user.valid_password?(password)
-    end
-    return unless authenticated == true
-    
-    # render content
-    render :text => "authenticated stuff"
-  end
-
+  #def update_one_authenticated
+  #
+  #end
+  
 protected
 
   def find_all_synchronizations
