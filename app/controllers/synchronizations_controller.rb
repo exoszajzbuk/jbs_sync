47a93ef31f1b9c966a730c4e3cdd4c6e07c4ea46
Balazs KOVACS <exoszajzbuk@gmail.com>
2012-02-22 18:07:21 +0100

    enh: added basic http auth

--- app/controllers/synchronizations_controller.rb
+++ app/controllers/synchronizations_controller.rb
@@ -3,7 +3,7 @@ class SynchronizationsController < ApplicationController
   before_filter :find_all_synchronizations
   before_filter :find_page
   
-  before_filter :check_model, :only => [:update_all, :delete_all]
+  before_filter :check_model, :only => [:update_all, :delete_all, :update_one]
 
   def index
     # you can use meta fields from your model instead (e.g. browser_title)
@@ -24,31 +24,61 @@ class SynchronizationsController < ApplicationController
   
   # update sync a whole model
   def update_all
-    if params[:updated_at].nil? then
-      @records = @model.all
+    if @model.needs_authentication? then
+      render :text => t('needs_authentication')
     else
-      @records = @model.find(:all, :conditions => ['updated_at > ?', Time.parse(params[:updated_at])+1])
-    end
       
-    respond_with_records @records
+      if params[:updated_at].nil? then
+        @records = @model.all
+      else
+        @records = @model.find(:all, :conditions => ['updated_at > ?', Time.parse(params[:updated_at])+1])
+      end
+      
+      respond_with_records @records
+    end
   end
   
   # delete sync a whole model
   def delete_all
-    @records = @model.all.collect{ |m| m.id }
+    if @model.needs_authentication? then
+      render :text => t('needs_authentication')
+    else
+      @records = @model.all.collect{ |m| m.id }
     
-    respond_with_records @records
+      respond_with_records @records
+    end
   end
   
   # down sync one record
-  #def update_single
-  #  @record = @model.find(params[:id])
-  #  respond_to do |format|
-  #    format.html { render :text => t('format_error') }
-  #    format.json { render :json => @record }
-  #    format.xml { render :xml => @record }
-  #  end
-  #end
+  def update_one
+    if @model.needs_authentication? then
+      update_all_authenticated
+    else
+      update_one_unauthenticated
+    end
+  end
+  
+  def update_one_unauthenticated
+    @record = @model.find(params[:id])
+    respond_to do |format|
+      format.html { render :text => t('format_error') }
+      format.json { render :json => @record }
+      format.xml { render :xml => @record }
+    end
+  end
+  
+  def update_all_authenticated
+    user = User.find(params[:id])
+    
+    # authenticate
+    authenticated = authenticate_or_request_with_http_basic "Authentication Required" do |username, password|
+      username == user.username && user.valid_password?(password)
+    end
+    return unless authenticated == true
+    
+    # render content
+    render :text => "authenticated stuff"
+  end
 
 protected
 
